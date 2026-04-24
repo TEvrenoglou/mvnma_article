a2cf62ae901a96b2c4846ed8a5a80aa55e2efbc2
Dr. Theodoros Evrenoglou <theodoros.evrenoglou@uniklinik-freiburg.de>
2026-04-24 19:47:24 +0200

    Update analysis for spiechart method

--- psoriasis_supplement/analysis.R
+++ psoriasis_supplement/analysis.R
@@ -13,15 +13,6 @@ library(mvnma)
 library(tidyverse)
 library(ggplot2)
 
-
-#
-# Code obtained from the original publication by Daly et al. (2020)
-# https://pubmed.ncbi.nlm.nih.gov/33115431/
-#
-
-source("funcs/code_spie_chart.R")
-
-
 #
 # Load SUCRA values as obtained from Table 4 in Daly et al. (2020)
 #
@@ -42,18 +33,5 @@ res
 # Calculate spie area for all treatments across outcomes
 #
 
-x <- s <- spie_area <- vector("list")
-
-for (i in 1:nrow(dat)) {
-  x[[i]] <- unname(unlist(c(dat[i,])))
-  
-  s[[i]] <- spie.chart(outcome = x[[i]],
-                       theta = rep(0.5 * pi, 4),
-                       outcome.range = c(0, 1),
-                       outcome.label = paste("Outcome", 1:4))
-  spie_area[[i]] <-
-    as.numeric(str_trim(gsub("Area inside spie chart = ", "", s[[i]][[2]])))
-  names(spie_area[[i]]) <- row.names(dat)[i]
-}
-#
-unlist(spie_area)
+res1 <- mvnma:::spiechart_internal(dat,weights = NULL)
+res1
